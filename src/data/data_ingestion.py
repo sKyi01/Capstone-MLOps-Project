3ccff389e5bed409e7db4ab2108070b97c4c7540
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2026-03-08 00:42:43 +0530

    Cred Updated

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -76,7 +76,7 @@ def main():
         # test_size = 0.2
         
         df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
-        # s3 = s3_connection.s3_operations("capstone-data-s3", "AKIA4J4B4ZP4EKNMNZFX", "qPwW5Vao7ytNTS32J8AuPsMs3VMrZ9VobepT9s9q")
+        # s3 = s3_connection.s3_operations("capstone-data-s3", "<accesskey>", "<accessPass>")
         # df = s3.fetch_file_from_s3("data.csv")
 
 
